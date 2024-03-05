c84bdb4a73ebacf55f95949668128bd19fa927cc
Anderson Farias <31108276+andersonfariiass@users.noreply.github.com>
2024-03-05 15:59:23 -0300

    ajustando o script para trabalhar com mais de um cluster ecs

--- ecs-service-schedule-stop.py
+++ ecs-service-schedule-stop.py
@@ -7,29 +7,35 @@ def get_cluster_name_with_tag(tag_key, tag_value):
         print("Buscando clusters...")
         paginator = ecs_client.get_paginator('list_clusters')
         response_iterator = paginator.paginate()
+        cluster_names=[]
 
         for page in response_iterator:
             cluster_arns = page['clusterArns']
-            for arn in cluster_arns:
-                cluster_info = ecs_client.describe_clusters(
-                    clusters=[arn],
-                    include=['TAGS']
-                )
-                cluster_tags = cluster_info['clusters'][0].get('tags', [])
-                print(f"Cluster ARN: {arn}")
+            print(cluster_arns)
+            cluster_info = ecs_client.describe_clusters(
+                clusters=cluster_arns,
+                include=['TAGS']
+            )
+            print(cluster_info)
+            for cluster in cluster_info['clusters']:
+                name = cluster['clusterName']
+                cluster_tags = cluster.get('tags', [])
+                print(f"Cluster Name: {name}")
                 print(f"Cluster tags: {cluster_tags}")
                 
                 # Verifica se a tag está presente e tem o valor correto
                 if any(tag['key'] == tag_key and tag['value'] == tag_value for tag in cluster_tags):
                     print(f"Cluster encontrado com a tag '{tag_key}={tag_value}'!")
-                    return cluster_info['clusters'][0]['clusterName']
+                cluster_names.append(name)
+                print(cluster_names)
+            return cluster_names
         
         print(f"Nenhum cluster encontrado com a tag '{tag_key}={tag_value}'.")
         return None
     except Exception as e:
         print(f"Erro ao buscar o cluster: {str(e)}")
         return None
-
+        
 def list_cluster_services(cluster_name):
     ecs_client = boto3.client('ecs')
     
@@ -86,29 +92,29 @@ def update_ecs_services(cluster_name, services, desired_count):
             cluster=cluster_name,
             service=service,
             desiredCount=desired_count
+            print(f"Número de réplicas do '{service}' no cluster '{name}' atualizado para {desired_count} réplicas")
         )
 
 def lambda_handler(event, context):
     tag_key = 'Stop'
     tag_value = 'True'
-    cluster_name = get_cluster_name_with_tag(tag_key, tag_value)  # Substitua pelo nome do seu cluster ECS22
-    #services = ['teste']  # Substitua pelos nomes dos seus serviços ECS
-    services = list_cluster_services(cluster_name)
-    desired_count = 0  # Valor padrão para parar o serviço
-
-    #Coleta o status atual do service
-    replica_counts = get_ecs_service_replica_count(cluster_name, services)
-    for service, replica_count in replica_counts.items():
-        print(f"Número de réplicas em execução para o serviço '{service}' no cluster '{cluster_name}': {replica_count}")
-
-    if replica_counts:
-        success = store_replica_counts_in_dynamodb(replica_counts)
-        if success:
-            print("Informações armazenadas com sucesso no DynamoDB.")
+    desired_count = 0
+    cluster_names = get_cluster_name_with_tag(tag_key, tag_value)
+    for name in cluster_names:
+        services = list_cluster_services(name)
+        #Coleta o status atual do service
+        replica_counts = get_ecs_service_replica_count(name, services)
+        for service, replica_count in replica_counts.items():
+            print(f"Número de réplicas em execução para o serviço '{service}' no cluster '{name}': {replica_count}")
+    
+        if replica_counts:
+            success = store_replica_counts_in_dynamodb(replica_counts)
+            if success:
+                print("Informações armazenadas com sucesso no DynamoDB.")
+            else:
+                print("Falha ao armazenar informações no DynamoDB.")
         else:
-            print("Falha ao armazenar informações no DynamoDB.")
-    else:
-        print("Não foi possível obter as contagens de réplicas dos serviços.")
+            print("Não foi possível obter as contagens de réplicas dos serviços.")
 
-    # Atualiza os serviços do ECS com a contagem desejada
-    update_ecs_services(cluster_name, services, desired_count)
+        # Atualiza os serviços do ECS com a contagem desejada
+        update_ecs_services(name, services, desired_count)
