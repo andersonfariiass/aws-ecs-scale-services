bf6cdb1067d4a1f0c428171d2f019d6e7cee5e25
Anderson Farias <31108276+andersonfariiass@users.noreply.github.com>
2024-03-07 12:37:48 -0300

    Update and rename ecs-service-schedule-stop.py to ecs-service-scale-down.py

--- ecs-service-scale-down.py
+++ ecs-service-scale-down.py
@@ -11,25 +11,23 @@ def get_cluster_name_with_tag(tag_key, tag_value):
 
         for page in response_iterator:
             cluster_arns = page['clusterArns']
-            print(cluster_arns)
             cluster_info = ecs_client.describe_clusters(
                 clusters=cluster_arns,
                 include=['TAGS']
             )
-            print(cluster_info)
             for cluster in cluster_info['clusters']:
                 name = cluster['clusterName']
                 cluster_tags = cluster.get('tags', [])
-                print(f"Cluster Name: {name}")
-                print(f"Cluster tags: {cluster_tags}")
                 
                 # Verifica se a tag está presente e tem o valor correto
                 if any(tag['key'] == tag_key and tag['value'] == tag_value for tag in cluster_tags):
                     print(f"Cluster encontrado com a tag '{tag_key}={tag_value}'!")
-                cluster_names.append(name)
+                    print(f"Cluster Name: {name}")
+                    print(f"Cluster tags: {cluster_tags}")
+                    cluster_names.append(name)
                 print(cluster_names)
             return cluster_names
-        
+            
         print(f"Nenhum cluster encontrado com a tag '{tag_key}={tag_value}'.")
         return None
     except Exception as e:
@@ -49,16 +47,17 @@ def list_cluster_services(cluster_name):
         print(f"Erro ao listar serviços: {str(e)}")
         return []
 
-def store_replica_counts_in_dynamodb(replica_counts):
+def store_replica_counts_in_dynamodb(replica_counts, cluster_name):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('ecs_services')  # Substitua 'NomeDaTabela' pelo nome da sua tabela no DynamoDB
+    table = dynamodb.Table('ecs_state_services')  # Substitua 'NomeDaTabela' pelo nome da sua tabela no DynamoDB
 
     try:
         for service, count in replica_counts.items():
             response = table.put_item(
                 Item={
                     'name_services': service,
-                    'replica_counts': count
+                    'replica_counts': count,
+                    'cluster_name': cluster_name
                 }
             )
             print(f"Informações armazenadas para '{service}': {response}")
@@ -86,14 +85,17 @@ def get_ecs_service_replica_count(cluster_name, services):
 
 def update_ecs_services(cluster_name, services, desired_count):
     ecs_client = boto3.client('ecs')
-
+    
     for service in services:
+        print(f"Realizando scale-down do serviço: '{service}'")
         response = ecs_client.update_service(
             cluster=cluster_name,
             service=service,
             desiredCount=desired_count
-            print(f"Número de réplicas do '{service}' no cluster '{name}' atualizado para {desired_count} réplicas")
         )
+        print(f"Número de réplicas do '{service}' no cluster '{cluster_name}' atualizado para {desired_count} réplicas")
+    print(f"scale-down no cluster '{cluster_name} finalizado'")
+    print()
 
 def lambda_handler(event, context):
     tag_key = 'Stop'
@@ -108,13 +110,14 @@ def lambda_handler(event, context):
             print(f"Número de réplicas em execução para o serviço '{service}' no cluster '{name}': {replica_count}")
     
         if replica_counts:
-            success = store_replica_counts_in_dynamodb(replica_counts)
+            success = store_replica_counts_in_dynamodb(replica_counts, name)
             if success:
+                update_ecs_services(name, services, desired_count)
                 print("Informações armazenadas com sucesso no DynamoDB.")
             else:
-                print("Falha ao armazenar informações no DynamoDB.")
+                print("Falha ao armazenar informações no DynamoDB, o update service não pode ser executado")
         else:
             print("Não foi possível obter as contagens de réplicas dos serviços.")
 
         # Atualiza os serviços do ECS com a contagem desejada
-        update_ecs_services(name, services, desired_count)
+        #update_ecs_services(name, services, desired_count)
